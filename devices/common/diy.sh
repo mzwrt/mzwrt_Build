e499a0c775018dab99661d3e9e5eea5db1f82fa2
mzal <58111339+mzwrt@users.noreply.github.com>
2024-12-04 14:35:06 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -2,7 +2,7 @@
 #=================================================
 shopt -s extglob
 
-sed -i '$a src-git MzWrt-packages https://github.com/mzwrt/MzWrt-packages.git;main' feeds.conf.default
+sed -i '$a src-git MzWrt-packages https://github.com/kiddin9/kwrt-packages.git;main' feeds.conf.default
 sed -i "/telephony/d" feeds.conf.default
 
 sed -i "s?targets/%S/packages?targets/%S/\$(LINUX_VERSION)?" include/feeds.mk
