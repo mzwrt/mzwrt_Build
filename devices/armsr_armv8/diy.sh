e0d247269b8b31892ec156a494a4eccdfea0709d
mzal <58111339+mzwrt@users.noreply.github.com>
2024-12-04 14:53:12 +0800

    Update diy.sh

--- devices/armsr_armv8/diy.sh
+++ devices/armsr_armv8/diy.sh
@@ -2,6 +2,6 @@
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf package/feeds/MzWrt-packages/rtl8188eu package/feeds/MzWrt-packages/rtl8192eu package/feeds/MzWrt-packages/rtl8812au-ac
+rm -rf package/feeds/MzWrt/rtl8188eu package/feeds/MzWrt-packages/rtl8192eu package/feeds/MzWrt-packages/rtl8812au-ac
 
 
